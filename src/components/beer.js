7bcdf7f6c7b3d081119435b7c9c2a71280631e9f
adigladi <adigladi@hotmail.com>
2019-11-05 21:49:30 +0100

    adds beer component

---
+++ src/components/beer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class beer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default beer
